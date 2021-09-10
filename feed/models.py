40cacf2e267c526d433d6eae854e69b406ff6fae
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-10 15:53:25 +0300

    register models in the model

--- feed/models.py
+++ feed/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 from django.db.models.fields import CharField, IntegerField, TextField
 from django.db.models.fields.files import ImageField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
-    image = ImageField(upload_to = 'images/')
-    name = CharField(max_length=200)
-    caption = TextField()
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length=200)
+    caption = models.TextField()
     profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
-    likes = IntegerField()
-    comments = TextField()
+    likes = models.IntegerField()
+    comments = models.TextField()
 
 
 
     def __str__(self):
         return self.name
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pp = models.ImageField(upload_to = 'images/')
+    bio = TextField()
+
+    def __str__(self):
+        return self.user.username
--- insta/urls.py
+++ insta/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('urls.feed'))
+    # path('', include('feed.urls'))
+    
 ]
