d4a229a8bb08849d0daafc605b71b9187c0cbbb1
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-11 08:55:15 +0300

    add templates and create url path for the home page

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def feed(request):
+    p = "it works"
+    context = {
+      'p':p 
+    }
+    return render(request, 'feed/feed.html',context)
