c991fe8917f18a66617c055af2c63ccd76edde52
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-12 09:23:41 +0300

    create a custom  registration form that inherits the inbuild Usercreation

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 # Create your views here.
+def register(request):
+    form = UserChangeForm()
+    context = {
+      'form':form
+    }
+    return render(request, 'users/register.html', context)
